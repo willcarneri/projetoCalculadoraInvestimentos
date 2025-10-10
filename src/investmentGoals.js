8d2f5a2e0871ea6a94e0984927f676ed425f979e
Willian Carneri <willianluigi@gmail.com>
2025-10-10 11:16:24 -0300

    primeira funcao auxiliar

---
+++ src/investmentGoals.js
@@ -0,0 +1,12 @@
+function convertToMonthlyReturnRate(yearlyRetunrRate) {
+  return yearlyRetunrRate ** (1 / 2);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = 'monthly',
+  montlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = 'monthly'
+) {}
